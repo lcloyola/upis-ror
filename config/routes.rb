8d7ee0750c26e98dce1f51501cbb25f83a4efeb4
lizbeth <lizbeth@lizbeth-desktop.(none)>
2012-01-18 21:55:29 +0800

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Upis::Application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    root :to => "home#index"
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

0360c0fb9408b1bca7ea09dec048cf4b1933f3a9
lizbeth <lizbeth@lizbeth-desktop.(none)>
2012-01-18 21:33:17 +0800

    rails new run

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upis::Application.config.secret_token = 'd4ce9188560694cc79772902d3447df8c463afbd7fe738e33b3de088467589ef1457f5a5e00dc52069b4eeacd8fb83ad90cdb2b40408283fb9abdfac0a179def'

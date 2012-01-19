34766ea104efa34fbcb9156d6fa509191ccf3113
Liz <chit@chit-1015PEM.(none)>
2012-01-19 10:39:39 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120118134306) do
+ActiveRecord::Schema.define(:version => 20120119023444) do
+
+  create_table "batches", :force => true do |t|
+    t.integer  "year"
+    t.integer  "remarks"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false

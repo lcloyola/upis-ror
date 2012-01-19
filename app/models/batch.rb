80668c96c2722bef4d3d9735a426c2d87975c013
Liz <chit@chit-1015PEM.(none)>
2012-01-19 10:54:28 +0800

    validate year value

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,2 +1,4 @@
 class Batch < ActiveRecord::Base
+    validates_numericality_of :year, :only_integer => true, :message => "can only be whole number."
+    validates_inclusion_of :year, :in => 1908..3000, :message => "can only be between 1908 and 3000." 
 end

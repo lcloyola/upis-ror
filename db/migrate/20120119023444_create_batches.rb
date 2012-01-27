fca59ca3be82a459271070f004fef83f51d2863b
lizbeth <lizbeth@lizbeth-desktop.(none)>
2012-01-27 11:13:16 +0800

    fix remarks

--- db/migrate/20120119023444_create_batches.rb
+++ db/migrate/20120119023444_create_batches.rb
@@ -2,7 +2,7 @@ class CreateBatches < ActiveRecord::Migration
   def change
     create_table :batches do |t|
       t.integer :year
-      t.integer :remarks
+      t.string :remarks
 
       t.timestamps
     end

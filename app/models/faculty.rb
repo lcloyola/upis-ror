8c3aef644fc7b940dc6c1059c4f7dc9cdc8ab517
lizbeth <lizbeth@lizbeth-desktop.(none)>
2012-02-04 17:58:21 +0800

    define fullname attribute for students and faculty

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,4 +1,8 @@
 class Faculty < ActiveRecord::Base
   belongs_to :department
   has_many :sections
+  
+  def fullname
+    "#{last} #{given} #{middle}"
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ActiveRecord::Base
   belongs_to :batch
+  
+  def fullname
+    "#{last} #{given} #{middle}"
+  end
 end

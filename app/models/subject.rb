a4562cf3f89de9e0a7c5da38de92387cadf316af
lizbeth <lizbeth@lizbeth-desktop.(none)>
2012-02-05 00:48:59 +0800

    added validation on subjects

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,9 @@
 class Subject < ActiveRecord::Base
+  validates_inclusion_of :year, :in => 1..5, :message => "can only be from 1 and 5."
+  validates_numericality_of :year, :only_integer => true, :message => "can only be whole number."
+  validates_uniqueness_of :name, :scope => [:units, :year, :department_id]
+  validates_presence_of :department_id, :units, :year, :name
+  
   belongs_to :department
   has_many :courses
   

0099208134057a1b91ebfb2b40c11b3ca4352b7d
Liz <chit@chit-1015PEM.(none)>
2012-02-16 14:17:53 +0800

    temporarily disable validation to accept blank grade

--- app/models/enrollee.rb
+++ app/models/enrollee.rb
@@ -1,7 +1,7 @@
 class Enrollee < ActiveRecord::Base
   validates :course_id, :student_id,  :presence => true
-  validates_numericality_of :quartera, :quarterb, :only_integer => true, :message => "can only be whole number."
-  validates_inclusion_of :quartera, :quarterb, :in => 0..100, :message => "can only be between 0 and 100."
+  #validates_inclusion_of :quartera, :quarterb, :in => 0..100, :message => "can only be between 0 and 100."
+  #validates_numericality_of :quartera, :quarterb, :only_integer => true, :message => "can only be whole number."
   attr_accessible :course_id, :student_id, :quartera, :quarterb, :id
   belongs_to :course
   belongs_to :student

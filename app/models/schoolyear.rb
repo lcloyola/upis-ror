172cb37740ec91e20b4660c8fc024c0fb1ac4eec
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-02-22 21:13:33 +0800

    defined current_schoolyear

--- app/models/schoolyear.rb
+++ app/models/schoolyear.rb
@@ -2,6 +2,7 @@ class Schoolyear < ActiveRecord::Base
   validates_numericality_of :start, :only_integer => true, :message => "can only be whole number."
   validates_inclusion_of :start, :in => 1908..3000, :message => "can only be between 1908 and 3000."
   validates_uniqueness_of :start
+  scope :current_schoolyear, :conditions => ['current = ?', true]
   
   has_many:sections
   has_many :courses

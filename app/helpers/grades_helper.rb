cf10dd822b992b377b270ec8b66a7dba287761e7
Lizbeth Loyola <lizbeth@lizbeth-desktop.(none)>
2012-04-06 13:17:34 +0800

    added eleven point system equivalent

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -1,2 +1,20 @@
 module GradesHelper
+
+  def elevenpt(average)
+    final = case average
+      when 96..100 then 1.0
+      when 91..95 then 1.25
+      when 86..90 then 1.50
+      when 80..85 then 1.75
+      when 74..79 then 2.00
+      when 68..73 then 2.25
+      when 62..67 then 2.50
+      when 56..61 then 2.50
+      when 50..55 then 3.00
+      when 40..49 then 4.00
+      else 5.00
+    end
+    return final
+  end
+  
 end

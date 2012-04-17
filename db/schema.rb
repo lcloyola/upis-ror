7eb80bb8b5df11f143b4f04f0dc4b0d9ad7509eb
Lizbeth Loyola <lizbeth@lizbeth-K43E.(none)>
2012-04-18 07:15:23 +0800

    some view changes

--- db/schema.rb
+++ db/schema.rb
@@ -14,21 +14,21 @@
 ActiveRecord::Schema.define(:version => 20120407095114) do
 
   create_table "batches", :force => true do |t|
-    t.integer   "year"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
-    t.string    "remarks"
+    t.integer  "year"
+    t.string   "remarks"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "courses", :force => true do |t|
-    t.integer   "subject_id"
-    t.integer   "faculty_id"
-    t.integer   "schoolyear_id"
-    t.integer   "sem"
-    t.integer   "section_id"
-    t.timestamp "created_at",                      :null => false
-    t.timestamp "updated_at",                      :null => false
-    t.boolean   "yearlong",      :default => true
+    t.integer  "subject_id"
+    t.integer  "faculty_id"
+    t.integer  "schoolyear_id"
+    t.integer  "sem"
+    t.integer  "section_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "yearlong",      :default => true
   end
 
   add_index "courses", ["faculty_id"], :name => "index_courses_on_faculty_id"
@@ -37,18 +37,18 @@ ActiveRecord::Schema.define(:version => 20120407095114) do
   add_index "courses", ["subject_id"], :name => "index_courses_on_subject_id"
 
   create_table "departments", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "enrollees", :force => true do |t|
-    t.integer   "course_id"
-    t.integer   "student_id"
-    t.integer   "quartera"
-    t.integer   "quarterb"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.integer  "course_id"
+    t.integer  "student_id"
+    t.integer  "quartera"
+    t.integer  "quarterb"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "enrollees", ["course_id", "student_id"], :name => "index_enrollees_on_course_id_and_student_id", :unique => true
@@ -56,38 +56,38 @@ ActiveRecord::Schema.define(:version => 20120407095114) do
   add_index "enrollees", ["student_id"], :name => "index_enrollees_on_student_id"
 
   create_table "faculties", :force => true do |t|
-    t.string    "last"
-    t.string    "given"
-    t.string    "middle"
-    t.integer   "department_id"
-    t.date      "appointment"
-    t.string    "email"
-    t.string    "mobile"
-    t.timestamp "created_at",    :null => false
-    t.timestamp "updated_at",    :null => false
-    t.date      "birthday"
-    t.string    "landline"
+    t.string   "last"
+    t.string   "given"
+    t.string   "middle"
+    t.integer  "department_id"
+    t.date     "appointment"
+    t.string   "email"
+    t.string   "mobile"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.date     "birthday"
+    t.string   "landline"
   end
 
   add_index "faculties", ["department_id"], :name => "index_faculties_on_department_id"
 
   create_table "grades", :force => true do |t|
-    t.integer   "course_id"
-    t.integer   "student_id"
-    t.integer   "quarter"
-    t.integer   "value"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.integer  "course_id"
+    t.integer  "student_id"
+    t.integer  "quarter"
+    t.integer  "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "grades", ["course_id"], :name => "index_grades_on_course_id"
   add_index "grades", ["student_id"], :name => "index_grades_on_student_id"
 
   create_table "members", :force => true do |t|
-    t.integer   "section_id"
-    t.integer   "student_id"
-    t.timestamp "created_at", :null => false
-    t.timestamp "updated_at", :null => false
+    t.integer  "section_id"
+    t.integer  "student_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "members", ["section_id"], :name => "index_members_on_section_id"
@@ -105,21 +105,21 @@ ActiveRecord::Schema.define(:version => 20120407095114) do
   add_index "removals", ["student_id"], :name => "index_removals_on_student_id"
 
   create_table "schoolyears", :force => true do |t|
-    t.integer   "start"
-    t.text      "remarks"
-    t.boolean   "current",    :default => false
-    t.timestamp "created_at",                    :null => false
-    t.timestamp "updated_at",                    :null => false
+    t.integer  "start"
+    t.text     "remarks"
+    t.boolean  "current",    :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "sections", :force => true do |t|
-    t.integer   "schoolyear_id"
-    t.integer   "faculty_id"
-    t.integer   "batch_id"
-    t.integer   "year"
-    t.string    "name"
-    t.timestamp "created_at",    :null => false
-    t.timestamp "updated_at",    :null => false
+    t.integer  "schoolyear_id"
+    t.integer  "faculty_id"
+    t.integer  "batch_id"
+    t.integer  "year"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "sections", ["batch_id"], :name => "index_sections_on_batch_id"
@@ -127,65 +127,65 @@ ActiveRecord::Schema.define(:version => 20120407095114) do
   add_index "sections", ["schoolyear_id"], :name => "index_sections_on_schoolyear_id"
 
   create_table "students", :force => true do |t|
-    t.integer   "student_no"
-    t.integer   "batch_id"
-    t.string    "last"
-    t.string    "given"
-    t.string    "middle"
-    t.string    "nick"
-    t.text      "address"
-    t.date      "birthday"
-    t.string    "mobile"
-    t.string    "landline"
-    t.string    "email"
-    t.string    "religion"
-    t.string    "entry"
-    t.boolean   "graduate"
-    t.boolean   "upcat"
-    t.boolean   "disciplinary"
-    t.string    "father"
-    t.string    "foccupation"
-    t.string    "foffice"
-    t.string    "fcontact"
-    t.string    "mother"
-    t.string    "moccupation"
-    t.string    "moffice"
-    t.string    "mcontact"
-    t.string    "guardian"
-    t.string    "relationship"
-    t.string    "gaddress"
-    t.string    "gcontact"
-    t.timestamp "created_at",   :null => false
-    t.timestamp "updated_at",   :null => false
+    t.integer  "student_no"
+    t.integer  "batch_id"
+    t.string   "last"
+    t.string   "given"
+    t.string   "middle"
+    t.string   "nick"
+    t.text     "address"
+    t.date     "birthday"
+    t.string   "mobile"
+    t.string   "landline"
+    t.string   "email"
+    t.string   "religion"
+    t.string   "entry"
+    t.boolean  "graduate"
+    t.boolean  "upcat"
+    t.boolean  "disciplinary"
+    t.string   "father"
+    t.string   "foccupation"
+    t.string   "foffice"
+    t.string   "fcontact"
+    t.string   "mother"
+    t.string   "moccupation"
+    t.string   "moffice"
+    t.string   "mcontact"
+    t.string   "guardian"
+    t.string   "relationship"
+    t.string   "gaddress"
+    t.string   "gcontact"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "students", ["batch_id"], :name => "index_students_on_batch_id"
 
   create_table "subjects", :force => true do |t|
-    t.string    "name"
-    t.integer   "department_id"
-    t.integer   "year"
-    t.timestamp "created_at",                                   :null => false
-    t.timestamp "updated_at",                                   :null => false
-    t.decimal   "units",         :precision => 10, :scale => 2
+    t.string   "name"
+    t.integer  "department_id"
+    t.decimal  "units",         :precision => 10, :scale => 0
+    t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "subjects", ["department_id"], :name => "index_subjects_on_department_id"
 
   create_table "users", :force => true do |t|
-    t.string    "email",                                 :default => "",    :null => false
-    t.string    "encrypted_password",     :limit => 128, :default => "",    :null => false
-    t.string    "reset_password_token"
-    t.timestamp "reset_password_sent_at",                                   :null => false
-    t.timestamp "remember_created_at",                                      :null => false
-    t.integer   "sign_in_count",                         :default => 0
-    t.timestamp "current_sign_in_at",                                       :null => false
-    t.timestamp "last_sign_in_at",                                          :null => false
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at",                                               :null => false
-    t.timestamp "updated_at",                                               :null => false
-    t.boolean   "admin",                                 :default => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "admin",                                 :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

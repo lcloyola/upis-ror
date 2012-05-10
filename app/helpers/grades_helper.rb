848f4d1c51e22cf567cbbca5297014eea9bad554
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-05-10 14:48:35 +0800

    transmutation table revision

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -4,15 +4,15 @@ module GradesHelper
     grade = average
     final = case grade
       when 96..100 then 1.0
-      when 91..95.99 then 1.25
-      when 86..90.99 then 1.50
-      when 80..85.99 then 1.75
-      when 74..79.99 then 2.00
-      when 68..73.99 then 2.25
-      when 62..67.99 then 2.50
-      when 56..61.99 then 2.50
-      when 50..55.99 then 3.00
-      when 40..49.99 then 4.00
+      when 91..95 then 1.25
+      when 86..90 then 1.50
+      when 80..85 then 1.75
+      when 74..79 then 2.00
+      when 68..73 then 2.25
+      when 62..67 then 2.50
+      when 56..61 then 2.75
+      when 50..55 then 3.00
+      when 40..49 then 4.00
       else 5.00
     end
     return final

47ddb74eb1deb22e84cb97a59e729db2887e310a
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-05-31 22:20:58 +0800

    refactor grading/class sheet code.

--- app/models/course.rb
+++ app/models/course.rb
@@ -19,21 +19,7 @@ class Course < ActiveRecord::Base
   }
 
   def my_students
-    return Grade.where('course_id = ?', self.id).group("student_id")
-  end
-
-  def student_average(student_id)
-    @grades = Grade.where('student_id = ? AND course_id = ?', student_id, self.id).sum('value')
-    return 0 if self.subject.is_pe?
-    if self.yearlong
-      return (@grades / 4.00).round
-    else
-      return (@grades / 2.00).round
-    end
-  end
-  def student_final(student_id)
-    return 0 if self.subject.is_pe?
-    return elevenpt(self.student_average(student_id))
+    return Grade.where('course_id = ?', self.id).group_by(&:student)
   end
   def semname
     if self.sem == 1

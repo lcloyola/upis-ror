97011b4fe4a1df0ce6ebc67791442c4f7cf3a891
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-07-26 13:09:23 +0800

    dont allow class without section

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,7 @@ class Course < ActiveRecord::Base
   validates_inclusion_of :sem, :in => 1..2, :message => "can only be from 1 or 2."
   validates_numericality_of :sem, :only_integer => true, :message => "can only be from 1 or 2."
   validates_uniqueness_of :section_id, :scope => [:schoolyear_id, :subject_id, :sem]
-  validates_presence_of :schoolyear_id, :faculty_id, :subject_id, :sem
+  validates_presence_of :schoolyear_id, :faculty_id, :subject_id, :sem, :section_id
 
   belongs_to :subject
   belongs_to :faculty

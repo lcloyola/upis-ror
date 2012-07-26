1fa6bee59beecbdefc6020023a4abdbad77f55e4
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-07-26 16:26:31 +0800

    change number of current batches

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,8 +2,9 @@ class Batch < ActiveRecord::Base
     validates_numericality_of :year, :only_integer => true, :message => "can only be whole number."
     validates_inclusion_of :year, :in => 1908..3000, :message => "can only be between 1908 and 3000."
     validates_uniqueness_of :year
-    scope :current, :conditions => ['year <= ? AND year > ?', Schoolyear.current_schoolyear.first.start + 4, Schoolyear.current_schoolyear.first.start]
-    
+    scope :current, :conditions => ['year <= ? AND year > ?', Schoolyear.current_schoolyear.first.start + 13, Schoolyear.current_schoolyear.first.start]
+
     has_many:students
     has_many:sections
 end
+

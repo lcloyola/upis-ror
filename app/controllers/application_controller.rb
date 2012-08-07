e874dab44e81548e26c669b28c52d2c4f9a1e77f
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-08-07 19:21:19 +0800

    do not allow inactive users to log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter :user_active!
   layout :choose_layout
 
   def choose_layout
@@ -11,6 +12,15 @@ class ApplicationController < ActionController::Base
     elsif current_user.role == Role::Faculty
       "faculty"
     end
+  end
+
+  def user_active!
+    if user_signed_in? && !current_user.is_active?
+      sign_out(current_user)
+      redirect_to root_url
+      return false
+    end
+    return true
   end
 end
 

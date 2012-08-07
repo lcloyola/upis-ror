94d906263741d413600b000b46e9ddd789f5575a
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-08-07 16:07:02 +0800

    switch association of faculty and user user has_one faculty, faculty :belongs_to user

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -2,11 +2,17 @@ class Faculty < ActiveRecord::Base
   belongs_to :department
   has_many :sections
   has_many :courses
-  has_one :user
+  belongs_to :user
   validates_presence_of :department_id, :last, :given
 
   def fullname
     "#{last} #{given} #{middle}"
+  end
+  def email
+    if self.user.present?
+      return self.user.email
+    end
+    return ""
   end
 end
 

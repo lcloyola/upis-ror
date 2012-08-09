d3eff5eba4653ad66f7ecbf86c64a9d9d69ac489
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-08-10 02:16:29 +0800

    validate uniqueness of removal grade

--- app/models/removal.rb
+++ app/models/removal.rb
@@ -1,6 +1,7 @@
 class Removal < ActiveRecord::Base
   belongs_to :course
   belongs_to :student
+  validates_uniqueness_of :course_id, :scope => [:student_id]
 
   def value
     return "(3.0)" if self.pass

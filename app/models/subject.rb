4871a043577681c7acadc5c728f858ed0159e766
Lizbeth Loyola <loyolalizbeth@gmail.com>
2014-07-02 18:12:20 +0800

    added work program type to subjects

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,5 @@
 class Subject < ActiveRecord::Base
-  validates_inclusion_of :year, :in => -1..12, :message => "can only be from 0 and 12."
+  validates_inclusion_of :year, :in => -2..12, :message => "can only be from -2 and 12."
   validates_numericality_of :year, :only_integer => true, :message => "can only be whole number."
   validates_uniqueness_of :name, :scope => [:units, :year, :department_id]
   validates_presence_of :department_id, :units, :year, :name

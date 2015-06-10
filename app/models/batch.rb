c51c1d59ece77f2229188b7951cd9de916558bc6
Lizbeth Loyola <loyolalizbeth@gmail.com>
2015-06-10 11:03:07 +0800

    More styling changes

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,9 +2,9 @@ class Batch < ActiveRecord::Base
     validates_numericality_of :year, :only_integer => true, :message => "can only be whole number."
     validates_inclusion_of :year, :in => 1908..3000, :message => "can only be between 1908 and 3000."
     validates_uniqueness_of :year
-    scope :current, :conditions => ['year <= ? AND year >= ?', Schoolyear.current.start + 13, Schoolyear.current.start]
+    scope :current, :conditions => ['year >=? AND year <= ?', Schoolyear.current.start, Schoolyear.current.start + 3]
+
 
     has_many:students
     has_many:sections
 end
-
